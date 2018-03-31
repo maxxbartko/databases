a74e655258910f4835c25f03747b9c5f4ed07766
maxxbartko <maxx.bartko@gmail.com>
2018-03-30 19:47:34 -0700

    commented out foreign keys; database wit htables created

--- server/schema.sql
+++ server/schema.sql
@@ -2,27 +2,27 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE chat.messages (
+CREATE TABLE messages (
   /* Describe your table here.*/
   id INT,
   user_id INT,
   text TEXT,
   room_id INT,
   created_at DATETIME,
-  PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (room_id) REFERENCES rooms(id)
+  PRIMARY KEY (id)
+  -- FOREIGN KEY (user_id) REFERENCES users(id),
+  -- FOREIGN KEY (room_id) REFERENCES rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
-CREATE TABLE chat.users (
-  id INT,
+CREATE TABLE users (
+  id INT NOT NULL,
   user_name TEXT,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE chat.rooms (
-  id INT,
+CREATE TABLE rooms (
+  id INT NOT NULL,
   room_name TEXT,
   PRIMARY KEY (id)
 );
