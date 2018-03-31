07a2d7a592b40560ed8913f96173bd62a25b1267
maxxbartko <maxx.bartko@gmail.com>
2018-03-30 20:24:23 -0700

    night push! figuring stuff out

--- server/db/index.js
+++ server/db/index.js
@@ -3,5 +3,39 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database : "chat"
+});
+
+con.connect(function(err) {
+    if (err) {
+        throw err;
+    }
+    console.log("connected!");
+//     con.query("SELECT * FROM customers", function (err, result, fields) {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+});
 
 
+
+// var mysql = require('mysql')
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'dbuser',
+//   password : 's3kreee7',
+//   database : 'my_db'
+// });
+
+// connection.connect()
+
+// connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+//   if (err) throw err
+
+//   console.log('The solution is: ', rows[0].solution)
+// })
+
+// connection.end()
--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,19 @@
 var db = require('../db');
-
+var app = require('../app.js');
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      app.get('/', function (req, res) {
+        res.sendfile("");
+      }) // a function which produces all the messages
+    },
+    post: function () { } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () { },
+    post: function () { }
   }
 };
 
