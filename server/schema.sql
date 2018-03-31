413721edacac6d86b21215c0390f4b54c4c7ddb0
maxxbartko <maxx.bartko@gmail.com>
2018-03-30 17:13:42 -0700

    made tables

--- server/schema.sql
+++ server/schema.sql
@@ -2,12 +2,30 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+CREATE TABLE chat.messages (
   /* Describe your table here.*/
+  id INT,
+  user_id INT,
+  text TEXT,
+  room_id INT,
+  created_at DATETIME,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE chat.users (
+  id INT,
+  user_name TEXT,
+  PRIMARY KEY (id)
+);
 
+CREATE TABLE chat.rooms (
+  id INT,
+  room_name TEXT,
+  PRIMARY KEY (id)
+);
 
 
 
